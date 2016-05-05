d71400fe1cf7d07214ba3d339f7131dd2a63f2e6
nattapat sukpootanan <nattapat.su@ku.th>
2016-05-05 16:23:26 +0700

    first commit

---
+++ android/src/com/group12/cookiesrising/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.group12.cookiesrising;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.group12.cookiesrising.Cookiesrising;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Cookiesrising(), config);
+	}
+}
