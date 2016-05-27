dcfb46e4bc66815162ca45a9867407378a78a60b
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-05-27 18:47:38 +0700

    Upgradable

--- core/src/com/group12/cookiesrising/Player.java
+++ core/src/com/group12/cookiesrising/Player.java
@@ -9,23 +9,30 @@ import com.group12.cookiesrising.gameobjects.Party;
  */
 public class Player {
 
-    private double dmg;
+    private double attackPoint;
     private double money;
+    private double healPoint;
+    private double criticalRate;
     public Party party;
 
     public Player(){
-        dmg = 1;
+        attackPoint = 1;
         //will load later;
         money = 0;
         party = new Party();
+        healPoint = 1;
+        criticalRate = 1;
     }
+    public void upgradeAtk(){ attackPoint++; }
+    public void upgradeHeal(){ healPoint++; }
+    public void upgradeCrt(){ criticalRate++; }
 
     public void addHero(Hero h){
         party.addHero(h);
     }
 
     public void attack(Enemy m){
-        m.takeDamage(dmg);
+        m.takeDamage(attackPoint);
     }
 
     public void takeMoney(double money){
@@ -37,6 +44,6 @@ public class Player {
     }
 
     public String getDamageText() {
-        return dmg+"";
+        return attackPoint+"";
     }
 }
