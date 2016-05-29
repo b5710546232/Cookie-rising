68efad0682acb93024f66acd52e526af192769ad
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-05-29 22:07:45 +0700

    Finish Health Bar for enemy

--- core/src/com/group12/cookiesrising/GameObjects/HealthBar.java
+++ core/src/com/group12/cookiesrising/GameObjects/HealthBar.java
@@ -1,15 +1,16 @@
 package com.group12.cookiesrising.gameobjects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.group12.cookiesrising.Health;
 import com.group12.cookiesrising.util.Assets;
 
 /**
  * Created by nattapat on 5/29/2016 AD.
  */
 public class HealthBar extends AbstractGameObject {
-    private Enemy enemy;
-    public HealthBar(Enemy e){
-        this.enemy =e;
+    private Health health;
+    public HealthBar(Health health){
+        this.health = health;
     }
     @Override
     public void update(float delta) {
@@ -18,7 +19,7 @@ public class HealthBar extends AbstractGameObject {
 
     @Override
     public void draw(SpriteBatch batch) {
-        batch.draw(Assets.hp_bg,300,300);
-        batch.draw(Assets.hp_knob,300,300);
+        batch.draw(Assets.hp_bg,258.5f,318f);
+        batch.draw(Assets.hp_knob,263.5f,323f,(float)(113*(health.getHp()/health.getMaxHp())),14);
     }
 }
