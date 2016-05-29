2e0ade16032496d733a385d2ce3d772c1bf431e9
nattapat sukpootanan <nattapat.su@ku.th>
2016-05-29 19:13:31 +0700

    create class hp bar

---
+++ core/src/com/group12/cookiesrising/GameObjects/HealthBar.java
@@ -0,0 +1,18 @@
+package com.group12.cookiesrising.gameobjects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by nattapat on 5/29/2016 AD.
+ */
+public class HealthBar extends AbstractGameObject {
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+
+    }
+}
