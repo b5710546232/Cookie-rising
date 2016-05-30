502cadd69a339bb3fb38f7b3bccccb2ffc16c5a2
nattapat sukpootanan <nattapat.su@ku.th>
2016-05-30 14:57:11 +0700

    fix dmgfactor

--- core/src/com/group12/cookiesrising/gametext/DamageTextFactory.java
+++ core/src/com/group12/cookiesrising/gametext/DamageTextFactory.java
@@ -3,18 +3,10 @@ package com.group12.cookiesrising.gametext;
 /**
  * Created by nattapat on 5/30/2016 AD.
  */
-<<<<<<< HEAD
 public class DamageTextFactory extends AbstractGameTextFactory{
 
     @Override
     public GameText create() {
         return new DamageText();
-=======
-public class DamageTextFactory extends AbstractGameTextFactory {
-    @Override
-    public GameText create() {
-        return null;
->>>>>>> b6925749588bbc42fcfaf563061593717c39cff4
     }
-
 }
